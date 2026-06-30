5757b7689f908a27ee059d4907f0038a91254a51
Marcelo-Alves719 <henmar5565@gmail.com>
2026-06-30 13:56:33 -0300

    Envio com o HistoricoSalarial e gerentes

---
+++ HistoricoSalario.cs
@@ -0,0 +1,21 @@
+﻿class HistoricoSalario
+{
+    
+  public static void hbjkh(){  
+        
+        Console.Write("Informe id do funcionario: ");
+        int id_funcionario = int.Parse(Console.ReadLine());
+
+        Console.Write("Informe seu salário anterior: ");
+        double salarioAnterior = double.Parse(Console.ReadLine());
+
+        Console.Write("Informe seu novo salário: ");
+        double novosalario = double.Parse(Console.ReadLine());
+
+        Console.Write("Informe sua data(mes e ano): ");
+        string data = Console.ReadLine();
+
+
+
+}
+}
---
+++ gerente.cs
@@ -0,0 +1,11 @@
+﻿class gerente
+{
+    public static void jdfh(){
+        
+        string nome;
+
+        Console.Write("Informe seu nome: ");
+        nome = Console.ReadLine();
+
+    }
+}
