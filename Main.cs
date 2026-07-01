901fe7f366c50ac3b936b2292ea6f5eb5f4266a4
Davi-Iuri <0001228156@senaimgaluno.com.br>
2026-07-01 15:18:11 -0300

    Main atualizada

--- Main.cs
+++ Main.cs
@@ -1,52 +1,81 @@
 ﻿
-class Main{
-public static void main(string[] args){
-
-
-int Cadastrar_Departamento = 0;
-int id = 0;
-int id_gerente = 0;
-int i = 0;
-
-Console.WriteLine("Bem-Vindo(a) ao cadastro, o que você deseja? \n 0-Sair \n 1-Cadastrar departamento");
-Cadastrar_Departamento=int.Parse(Console.ReadLine());
-
-do
+class Principal
 {
-    Console.WriteLine("Bem-Vindo(a) ao cadastro, o que você deseja? \n 0-Sair \n 1-Cadastrar departamento");
-    Cadastrar_Departamento=int.Parse(Console.ReadLine());
-    i++;
-}
-while(Cadastrar_Departamento>=2);
-
-switch (Cadastrar_Departamento){
-
-
-    case 1:
-    Console.WriteLine("Insira o id do departamento");
-    id=int.Parse(Console.ReadLine());
-    
-    Console.WriteLine("Insira o nome do departamento");
-    string nome = Console.ReadLine();
-    
-    Console.WriteLine("Insira o id do gerente do departamento");
-    id_gerente=int.Parse(Console.ReadLine());
-    
-    Console.WriteLine("Insira o ramal do departamento");
-    string ramal = Console.ReadLine();
-
-    break;
-    
-    case 0:
-    Console.WriteLine("Aplicativo fechado.");
-    break;
-
-    
-
-}
-
-
-
-
-}
+    static void Main(string[] args)
+    {
+        int id = 0;
+        int id_gerente = 0;
+        int Cadastro_Geral = 0;
+        string nome = "";
+        int idf = 0;
+        int id_funcionario = 0;
+        int matricula = 0;
+        string cpf = "";
+        string dataNascimento = "";
+        int idDepartamento = 0;
+        string nomed = "";
+        string ramal = "";
+        string endereco = "";
+        string telefone = "";
+        double salario = 0.0;
+        string NomeGerente = "";
+        double salarioAnterior = 0.0;
+        double novosalario = 0.0;
+        string data = "";
+        Console.WriteLine("Bem-Vindo(a) ao cadastro, o que você deseja? \n 0-Sair \n 1-Cadastrar departamento \n 2-Cadastrar Funcionário \n 3- Cadastrar Gerente \n 4- Historico de salário");
+        Cadastro_Geral = int.Parse(Console.ReadLine());
+        switch (Cadastro_Geral)
+        {
+            case 1:
+                Console.WriteLine("Insira o id do departamento");
+                id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Insira o nome do departamento");
+                nomed = Console.ReadLine();
+                Console.WriteLine("Insira o id do gerente do departamento");
+                id_gerente = int.Parse(Console.ReadLine());
+                Console.WriteLine("Insira o ramal do departamento");
+                ramal = Console.ReadLine();
+                break;
+            case 0:
+                Console.WriteLine("Aplicativo fechado.");
+                break;
+            case 2:
+                Console.WriteLine("Digite seu nome: ");
+                nome = Console.ReadLine(); //nome completo
+                Console.WriteLine("Digite seu id: ");//int
+                idf = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite sua matricula: ");//int
+                matricula = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite seu CPF: ");
+                cpf = Console.ReadLine();
+                Console.WriteLine("Digite sua Data de nascimento: "); //dia, mês e ano 
+                dataNascimento = Console.ReadLine();
+                Console.WriteLine("Digite seu id de Departamento: ");//int
+                idDepartamento = int.Parse(Console.ReadLine());
+                Console.WriteLine("Informe seu endereço: ");
+                endereco = Console.ReadLine(); //cep da rua e número da casa 
+                Console.WriteLine("Informe o seu número de telefone: ");
+                telefone = Console.ReadLine(); // apenas números
+                Console.WriteLine("Digite o valor do seu salário: ");//double
+                salario = double.Parse(Console.ReadLine());
+                break;
+            case 3:
+                Console.Write("Informe seu nome: ");
+                NomeGerente = Console.ReadLine();
+                break;
+            case 4:
+                Console.Write("Informe id do funcionario: ");
+                id_funcionario = int.Parse(Console.ReadLine());
+                Console.Write("Informe seu salário anterior: ");
+                salarioAnterior = double.Parse(Console.ReadLine());
+                Console.Write("Informe seu novo salário: ");
+                novosalario = double.Parse(Console.ReadLine());
+                Console.Write("Informe sua data(mes e ano): ");
+                data = Console.ReadLine();
+                break;
+            default:
+                Console.WriteLine("Nenhuma das opções disponível");
+                break;
+        }
+    }
 }
